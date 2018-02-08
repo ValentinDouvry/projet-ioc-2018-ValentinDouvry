5cf4d608a723fea6c5552b1dd1a11f989074d68b
Douvry <valentin.jabba@gmail.com>
2018-02-08 02:57:49 -0500

    Ajout fonctions

--- OutilMétéores/src/ca/qc/cgmatane/informatique/outilmeteore/vue/VueOutilMeteore.java
+++ OutilMétéores/src/ca/qc/cgmatane/informatique/outilmeteore/vue/VueOutilMeteore.java
@@ -1,13 +1,16 @@
 package ca.qc.cgmatane.informatique.outilmeteore.vue;
 import ca.qc.cgmatane.informatique.outilmeteore.action.ControleurOutilMeteores;
 import javafx.application.Application;
+import javafx.embed.swing.SwingNode;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-
+import javax.swing.*;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+
 
 
 public class VueOutilMeteore extends Application
@@ -21,12 +24,28 @@ public class VueOutilMeteore extends Application
 		this.controleur = new ControleurOutilMeteores(this);
 	}
 
-	@Override
-	public void start(Stage scenePrincipale)
+	public void start(Stage scenePrincipale) 
 	{
 		
+	}
+	
+	
+
+	public void afficherMarqueurMeteore(int id,int[] coordonnees)
+	{
 		
 	}
+	
+	public void inscrireDescriptionMeteore(int id, String texte)
+	{
+		
+	}
+	
+	public void afficherDescription(int id)
+	{
+		
+	}
+	
 	
 
 }
