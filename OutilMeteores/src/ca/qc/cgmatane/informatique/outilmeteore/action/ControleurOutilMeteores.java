d641ca3f0ee1a8546f858dd1478e4b35394392d7
DouvryValentin <valentin.jabba@gmail.com>
2018-02-15 22:37:12 -0500

    Ajout meteores de test

--- OutilMeteores/src/ca/qc/cgmatane/informatique/outilmeteore/action/ControleurOutilMeteores.java
+++ OutilMeteores/src/ca/qc/cgmatane/informatique/outilmeteore/action/ControleurOutilMeteores.java
@@ -23,7 +23,10 @@ public class ControleurOutilMeteores
 	protected Meteore meteoreTest1;
 	protected Meteore meteoreTest2;
 	protected Meteore meteoreTest3;
-	
+	protected Meteore meteoreTest4;
+	protected Meteore meteoreTest5;
+	protected Meteore meteoreTest6;
+	protected Meteore meteoreTest7;
 	
 	
 	
@@ -34,6 +37,11 @@ public class ControleurOutilMeteores
 		meteoreTest1 = new Meteore();
 		meteoreTest2 = new Meteore();
 		meteoreTest3 = new Meteore();
+		meteoreTest4 = new Meteore();
+		meteoreTest5 = new Meteore();
+		meteoreTest6 = new Meteore();
+		meteoreTest7 = new Meteore();
+		
 		listeMeteore = new ArrayList<Meteore>();
 		
 		meteoreTest1.setId(0);
@@ -48,9 +56,29 @@ public class ControleurOutilMeteores
 		meteoreTest3.setCoordonnees(new int[] {70,100});
 		meteoreTest3.setDescription("Description du meteore numero 3");
 		
+		meteoreTest4.setId(3);
+		meteoreTest4.setCoordonnees(new int[] {254,37});
+		meteoreTest4.setDescription("Description du meteore numero 4");
+		
+		meteoreTest5.setId(4);
+		meteoreTest5.setCoordonnees(new int[] {100,67});
+		meteoreTest5.setDescription("Description du meteore numero 5");
+		
+		meteoreTest6.setId(5);
+		meteoreTest6.setCoordonnees(new int[] {511,169});
+		meteoreTest6.setDescription("Description du meteore numero 6");
+		
+		meteoreTest7.setId(6);
+		meteoreTest7.setCoordonnees(new int[] {399,255});
+		meteoreTest7.setDescription("Description du meteore numero 7");
+		
 		listeMeteore.add(meteoreTest1);
 		listeMeteore.add(meteoreTest2);
-		listeMeteore.add(meteoreTest3);		
+		listeMeteore.add(meteoreTest3);
+		listeMeteore.add(meteoreTest4);	
+		listeMeteore.add(meteoreTest5);	
+		listeMeteore.add(meteoreTest6);	
+		listeMeteore.add(meteoreTest7);	
 		
 		Iterator<Meteore> visiteurMeteore = listeMeteore.iterator();
 		while(visiteurMeteore.hasNext())
